2e90d6833cb8754faf7b9f6bb1ec6459efc09bc2
Zamzam Fahiye <zamzam.fahiyee@outlook.com>
2023-11-08 11:45:52 +0100

    Initial commit

---
+++ src/Labb1_flights.java
@@ -0,0 +1,19 @@
+public class Labb1_flights {
+    public static void main(String[] args) {
+        // Uträkning för flyg från New York
+        System.out.println("******************************************************************");
+        System.out.println(); // Skapar en tom rad
+        System.out.println("Vilken tid anländer flyget från New York i Stockholm?");
+        System.out.println(); // Skapar en tom rad
+        System.out.println("Avgångstid i NY kl. " + 10 + ":" + 10 + ", anländer i Stockholm kl. " + 23 + ":" + 35);
+        System.out.println(); // Skapar en tom rad
+        System.out.println("******************************************************************");
+        System.out.println(); // Skapar en tom rad
+        // Uträkning för flyg från Stockholm
+        System.out.println("Vilken tid anländer flyget från stockholm i NY?");
+        System.out.println(); // Skapar en tom rad
+        System.out.println("Avgångstid från Stockholm kl. " + 14 + ":" + 03 + ", anländer i NY kl. " + 15 + ":" + 28 );
+        System.out.println(); // Skapar en tom rad
+        System.out.println("******************************************************************");
+    }
+}
