013b6c727aaf4b791db4995a7c0cd712a8f988dc
Zamzam Fahiye <zamzam.fahiyee@outlook.com>
2023-11-13 08:50:40 +0100

    Modifierats

--- src/Labb1_flights.java
+++ src/Labb1_flights.java
@@ -1,18 +1,20 @@
-public class Labb1_flights {
-    public static void main(String[] args) {
+public class Labb1_flights { // Här skapas en klass med namnet Labb1_flights
+    public static void main(String[] args) { // Här deklareras metoden main, där all kod befinner sig
         // Uträkning för flyg från New York
         System.out.println("******************************************************************");
         System.out.println(); // Skapar en tom rad
-        System.out.println("Vilken tid anländer flyget från New York i Stockholm?");
+        System.out.println("Vilken tid anländer flyget från New York i Stockholm?"); // Det här är en metod som skriver ut en sträng till konsolen
         System.out.println(); // Skapar en tom rad
-        System.out.println("Avgångstid i NY kl. " + 10 + ":" + 10 + ", anländer i Stockholm kl. " + 23 + ":" + 35);
+        System.out.println("Avgångstid i NY kl. " + (5 + 5) + ":" + (5 + 5) + ", anländer i Stockholm kl. " + (10 + 7 + 6) + ":" + (30 + 5)); /*
+       Här används samma metod för att göra en uträkning på avgångstiden och ankomstiden med åtanke på restid och tidsskillnad. */
         System.out.println(); // Skapar en tom rad
         System.out.println("******************************************************************");
         System.out.println(); // Skapar en tom rad
         // Uträkning för flyg från Stockholm
-        System.out.println("Vilken tid anländer flyget från stockholm i NY?");
+        System.out.println("Vilken tid anländer flyget från stockholm i NY?"); // Det här är en metod som skriver ut en sträng till konsolen
         System.out.println(); // Skapar en tom rad
-        System.out.println("Avgångstid från Stockholm kl. " + 14 + ":" + 03 + ", anländer i NY kl. " + 15 + ":" + 28 );
+        System.out.println("Avgångstid från Stockholm kl. " + (10 + 4) + ":" + 0 + 3 + ", anländer i NY kl. " + (14 + 7 - 6) + ":" + (3 + 25) ); /*
+       Här används samma metod för att göra en uträkning på avgångstiden och ankomstiden med åtanke på restid och tidsskillnad. */
         System.out.println(); // Skapar en tom rad
         System.out.println("******************************************************************");
     }
